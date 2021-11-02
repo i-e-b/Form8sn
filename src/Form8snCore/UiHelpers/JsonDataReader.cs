777965dddf0e2b862675763be8622a3282fb7040
Iain Ballard <iain.ballard@bjss.com>
2021-11-02 16:27:55 +0000

    initial box drawing

--- src/Form8snCore/UiHelpers/JsonDataReader.cs
+++ src/Form8snCore/UiHelpers/JsonDataReader.cs
@@ -1,78 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using Form8snCore.FileFormats;
 
 namespace Form8snCore.UiHelpers
 {
-
-    /// <summary>
-    /// NodeTree holds structured data along with descriptions, tags, and other display information.
-    /// </summary>
-    public class DataNode
-    {
-        /// <summary>
-        /// Child nodes of this node (can be empty)
-        /// </summary>
-        public List<DataNode> Nodes { get; set; } = new List<DataNode>();
-        
-        /// <summary>
-        /// Path through the object to reach this node as a string separated with a special char.
-        /// </summary>
-        public string DataPath { get; set; } = "";
-        
-        /// <summary>
-        /// Name of the node
-        /// </summary>
-        public string Name { get; set; } = "";
-        
-        /// <summary>
-        /// Standard color of this node (green for leaf nodes, grey for parent nodes, purple for arrays)
-        /// </summary>
-        public string ForeColor { get; set; } = "#000";
-        
-        /// <summary>
-        /// Description of the data at this node (node name plus sampled value)
-        /// </summary>
-        public string Text { get; set; } = "";
-
-        /// <summary>
-        /// Depth through the tree to this node
-        /// </summary>
-        public int Depth { get; set; }
-        
-        /// <summary>
-        /// True if the node has any child nodes. False if this is a leaf node.
-        /// </summary>
-        public bool HasChildren => Nodes.Count > 0;
-
-        /// <summary>
-        /// A unique ID for this node that can be used for HTML elements.
-        /// </summary>
-        public string HtmlId => DataPath.Replace(Strings.Separator, '-').Replace('[','_').Replace(']','_'); // TODO: optimise this
-
-        /// <summary>
-        /// A comma separated list of HtmlId for each direct child of this node
-        /// </summary>
-        public string ChildIds => "#"+string.Join(",#", Nodes.Select(n=>n.HtmlId)); // TODO: optimise this
-
-        /// <summary>
-        /// Set to true if this is a valid item to be selected by the user
-        /// </summary>
-        public bool CanBePicked { get; set; }
-
-        public DataNode()
-        {
-        }
-
-        public DataNode(string text, IEnumerable<DataNode> children)
-        {
-            Text = text;
-            Nodes.AddRange(children);
-        }
-
-    }
-
     /// <summary>
     /// JsonDataReader provides routines to read sample data into formats
     /// appropriate for UI display
@@ -102,7 +33,7 @@ namespace Form8snCore.UiHelpers
                     Depth = depth,
                     DataPath = path,
                     Name = name,
-                    ForeColor = "#777" //Color.DimGray,
+                    ForeColor = ColorGrey
                 });
             }
             else if (o is ArrayList array)
@@ -123,16 +54,17 @@ namespace Form8snCore.UiHelpers
                             Text = idxStr + " = <null>",
                             DataPath = path + Strings.Separator + idxStr,
                             Name = name,
-                            ForeColor = "#808" //Color.Purple,
+                            ForeColor = ColorPurple
                         });
                 }
 
                 outp.Add(new DataNode(node + " (multiple)", collection.ToArray())
                 {
                     Depth = depth,
+                    IsRepeated = true,
                     DataPath = path,
                     Name = name,
-                    ForeColor = "#808" //Color.Purple,
+                    ForeColor = ColorPurple
                 });
             }
             else
@@ -144,13 +76,17 @@ namespace Form8snCore.UiHelpers
                     Name = name,
                     Text = node + " = " + o,
                     CanBePicked = true,
-                    ForeColor = "#080" //Color.Green
+                    ForeColor = ColorGreen
                 });
             }
 
             return outp;
 
         }
+
+        private const string ColorGrey = "#777";
+        private const string ColorPurple = "#808";
+        private const string ColorGreen = "#080";
 
         public static List<DataNode> FlattenTree(List<DataNode> tree)
         {
