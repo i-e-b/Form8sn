b3f835b1dee53e2ed77c90b6d3cbd53539311d7d
Iain Ballard <iain.ballard@bjss.com>
2021-11-02 13:11:43 +0000

    Working picker

--- src/Form8snCore/UiHelpers/JsonDataReader.cs
+++ src/Form8snCore/UiHelpers/JsonDataReader.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Form8snCore.FileFormats;
 
 namespace Form8snCore.UiHelpers
@@ -10,12 +11,55 @@ namespace Form8snCore.UiHelpers
     /// </summary>
     public class DataNode
     {
+        /// <summary>
+        /// Child nodes of this node (can be empty)
+        /// </summary>
         public List<DataNode> Nodes { get; set; } = new List<DataNode>();
+        
+        /// <summary>
+        /// Path through the object to reach this node as a string separated with a special char.
+        /// </summary>
         public string DataPath { get; set; } = "";
+        
+        /// <summary>
+        /// Name of the node
+        /// </summary>
         public string Name { get; set; } = "";
+        
+        /// <summary>
+        /// Standard color of this node (green for leaf nodes, grey for parent nodes, purple for arrays)
+        /// </summary>
         public string ForeColor { get; set; } = "#000";
+        
+        /// <summary>
+        /// Description of the data at this node (node name plus sampled value)
+        /// </summary>
         public string Text { get; set; } = "";
+
+        /// <summary>
+        /// Depth through the tree to this node
+        /// </summary>
+        public int Depth { get; set; }
+        
+        /// <summary>
+        /// True if the node has any child nodes. False if this is a leaf node.
+        /// </summary>
         public bool HasChildren => Nodes.Count > 0;
+
+        /// <summary>
+        /// A unique ID for this node that can be used for HTML elements.
+        /// </summary>
+        public string HtmlId => DataPath.Replace(Strings.Separator, '-').Replace('[','_').Replace(']','_'); // TODO: optimise this
+
+        /// <summary>
+        /// A comma separated list of HtmlId for each direct child of this node
+        /// </summary>
+        public string ChildIds => "#"+string.Join(",#", Nodes.Select(n=>n.HtmlId)); // TODO: optimise this
+
+        /// <summary>
+        /// Set to true if this is a valid item to be selected by the user
+        /// </summary>
+        public bool CanBePicked { get; set; }
 
         public DataNode()
         {
@@ -38,10 +82,10 @@ namespace Form8snCore.UiHelpers
 
         public static List<DataNode> ReadObjectIntoNodeTree(object o, string path, string node)
         {
-            return ReadObjectRecursive(o, path, node);
+            return ReadObjectRecursive(o, path, node, 0);
         }
 
-        private static List<DataNode> ReadObjectRecursive(object o, string path, string node)
+        private static List<DataNode> ReadObjectRecursive(object o, string path, string node, int depth)
         {
             var name = string.IsNullOrWhiteSpace(path) ? "" : node;
             var outp = new List<DataNode>();
@@ -50,11 +94,12 @@ namespace Form8snCore.UiHelpers
                 var collection = new List<DataNode>();
                 foreach (var kvp in dict)
                 {
-                    collection.AddRange(ReadObjectRecursive(kvp.Value, path + Strings.Separator + kvp.Key, kvp.Key));
+                    collection.AddRange(ReadObjectRecursive(kvp.Value, path + Strings.Separator + kvp.Key, kvp.Key, depth + 1));
                 }
 
                 outp.Add(new DataNode(node, collection.ToArray())
                 {
+                    Depth = depth,
                     DataPath = path,
                     Name = name,
                     ForeColor = "#777" //Color.DimGray,
@@ -69,11 +114,12 @@ namespace Form8snCore.UiHelpers
                     var idxStr = $"[{index}]";
                     if (kvp != null)
                     {
-                        collection.AddRange(ReadObjectRecursive(kvp, path + Strings.Separator + idxStr, idxStr));
+                        collection.AddRange(ReadObjectRecursive(kvp, path + Strings.Separator + idxStr, idxStr, depth + 1));
                     }
                     else
                         collection.Add(new DataNode
                         {
+                            Depth = depth + 1,
                             Text = idxStr + " = <null>",
                             DataPath = path + Strings.Separator + idxStr,
                             Name = name,
@@ -83,6 +129,7 @@ namespace Form8snCore.UiHelpers
 
                 outp.Add(new DataNode(node + " (multiple)", collection.ToArray())
                 {
+                    Depth = depth,
                     DataPath = path,
                     Name = name,
                     ForeColor = "#808" //Color.Purple,
@@ -92,9 +139,11 @@ namespace Form8snCore.UiHelpers
             {
                 outp.Add(new DataNode
                 {
+                    Depth = depth,
                     DataPath = path,
                     Name = name,
                     Text = node + " = " + o,
+                    CanBePicked = true,
                     ForeColor = "#080" //Color.Green
                 });
             }
--- src/WebFormFiller/wwwroot/js/propertyInspector.js
+++ src/WebFormFiller/wwwroot/js/propertyInspector.js
@@ -1,27 +1,41 @@
-﻿function toggle(btnID, eIDs) {
+﻿function toggle(btnID, direction) {
     let i;
     
-    // Feed the list of ids as a selector
-    const theRows = document.querySelectorAll(eIDs);
     // Get the button that triggered this
-    const theButton = document.getElementById(btnID);
-    // If the button is not expanded...
-    if (theButton.getAttribute("aria-expanded") === "false") {
-        // Loop through the rows and show them
-        for (i = 0; i < theRows.length; i++) {
-            theRows[i].classList.add("shown");
-            theRows[i].classList.remove("hidden");
-        }
-        // Now set the button to expanded
-        theButton.setAttribute("aria-expanded", "true");
-        // Otherwise button is not expanded...
-    } else {
+    const button = document.getElementById(btnID);
+    if (!button) {return;}
+
+    const childRowIds = button.getAttribute("childIds");
+    const childRowElements = document.querySelectorAll(childRowIds);
+
+    // we specifically want to close, or the button is currently open
+    if (direction === "close" || button.getAttribute("aria-expanded") !== "false") {
         // Loop through the rows and hide them
-        for (i = 0; i < theRows.length; i++) {
-            theRows[i].classList.add("hidden");
-            theRows[i].classList.remove("shown");
+        for (i = 0; i < childRowElements.length; i++) {
+            // if this row is also collapsable, AND is expanded, collapse it.
+            if (childRowElements[i].classList.contains("shown")) {
+                let childBtnId = "btn-" + childRowElements[i].id;
+                let found = document.getElementById(childBtnId);
+                if (found && found.onclick) {
+                    toggle(childBtnId, "close");
+                }
+            }
+
+            // Hide the row
+            childRowElements[i].classList.add("hidden");
+            childRowElements[i].classList.remove("shown");
         }
         // Now set the button to collapsed
-        theButton.setAttribute("aria-expanded", "false");
+        button.setAttribute("aria-expanded", "false"); 
+    } else if (button.getAttribute("aria-expanded") === "false") {// The button is closed
+        // Show each row
+        for (i = 0; i < childRowElements.length; i++) {
+            childRowElements[i].classList.add("shown");
+            childRowElements[i].classList.remove("hidden");
+        }
+        // Update recorded state
+        button.setAttribute("aria-expanded", "true");
+    } else {
+        console.log(`Toggle binding error: ${btnID}`);
     }
 }
